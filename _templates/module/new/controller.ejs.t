3df4b4e5f136c761b21302494c527a4ad8cdd324
mwilk <wilk.michal@outlook.com>
2020-03-09 20:43:42 +0100

    feat: update endpoints, tests and documentation

---
+++ _templates/module/new/controller.ejs.t
@@ -0,0 +1,35 @@
+---
+to: src/api/<%= name %>/controllers/index.ts
+---
+import { Request, Response, NextFunction } from 'express'
+import { find, create } from '../repository'
+import { addSmile, mapSmiles } from '../services'
+
+export const getExamples = async (
+  _: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  try {
+    const examples = await find()
+    const examplesWithSmile = mapSmiles(examples)
+    return response.status(200).json(examplesWithSmile)
+  } catch (err) {
+    return next(err)
+  }
+}
+
+export const createExample = async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  try {
+    const { body } = request
+    await create(body)
+    const exampleWithSmile = addSmile(body)
+    return response.status(200).json(exampleWithSmile)
+  } catch (err) {
+    return next(err)
+  }
+}
--- src/api/examples/schemas/index.ts
+++ src/api/examples/schemas/index.ts
@@ -1,5 +1,46 @@
 import { object, string } from 'joi'
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     CreateExampleSchema:
+ *       type: object
+ *       properties:
+ *         text:
+ *           type: string
+ *           required: true
+ */
 export const CreateExampleSchema = object().keys({
+  text: string(),
+})
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     UpdateExampleSchema:
+ *       type: object
+ *       required:
+ *         - text
+ *       properties:
+ *         text:
+ *           type: string
+ */
+export const UpdateExampleSchema = object().keys({
+  text: string().required(),
+})
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     PatchExampleSchema:
+ *       type: object
+ *       properties:
+ *         text:
+ *           type: string
+ */
+export const PatchExampleSchema = object().keys({
   text: string(),
 })
