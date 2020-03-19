a5ce8a43e8af3f617aad9c5621ffa92eb4f59d08
mwilk <wilk.michal@outlook.com>
2020-03-19 18:24:32 +0100

    feat: add integration tests

--- src/loaders/middleware.ts
+++ src/loaders/middleware.ts
@@ -1,4 +1,4 @@
-import { Express } from 'express'
+import { Express, Request, Response, NextFunction } from 'express'
 import config, { swagger } from 'config'
 import cors from 'cors'
 import bodyParser from 'body-parser'
@@ -8,10 +8,11 @@ import morgan from 'morgan'
 import helmet from 'helmet'
 import api from 'api'
 
+const skip = (_: Request, __: Response, next: NextFunction) => next()
 const logger = () => {
   if (config.NODE_ENV === 'development') return morgan('dev')
   if (config.NODE_ENV === 'production') return morgan('combined')
-  return () => {}
+  return skip
 }
 
 const loadMiddleware = (app: Express) =>
